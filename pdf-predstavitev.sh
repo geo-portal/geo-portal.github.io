c45d2df263c66339938dd7661ff59a81dca02422
Uroš <uros00@gmail.com>
2019-10-13 20:10:12 +0200

    prepare for pdf

--- pdf-predstavitev.sh
+++ pdf-predstavitev.sh
@@ -4,11 +4,17 @@ echo 'Kill all Jekyll instances'
 kill -9 $(ps aux | grep '[j]ekyll' | awk '{print $2}')
 clear
 
+#uros: instanca 1 git bash zaženež serve
+
 echo "Building PDF-friendly HTML site for predstavitev ...";
 bundle exec jekyll serve --detach --config _config.yml,pdfconfigs/config_predstavitev_pdf.yml;
 echo "done";
+
+#uros: instanca 2 git bash zaženeš prince
 
 echo "Building the PDF ...";
 prince --javascript --input-list=_site/pdfconfigs/prince-list.txt -o pdf/predstavitev.pdf;
 
 echo "Done. Look in the pdf directory to see if it printed successfully."
+
+#uros: urediš pdf po potrebi https://www.pdfescape.com
