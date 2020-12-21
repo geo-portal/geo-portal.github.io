52736c2f09a64b99c9c3cdfa2e8402dda3aebdea
Uroš <uros00@gmail.com>
2020-12-21 22:29:42 +0100

    update for local setup

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 This is documentation source for https://site.geo-portal.si
+
+Local setup for testing and generating PDF (do not commit changes in Gemfile and Gemfile.lock!!)
+
+1. Edit Gemfile for local
+
+2. Run Git bash in project folder
+
+   `cd c:/Users/uros/PhpstormProjects/geo-portal.github.io`
+   
+   `bundle update`
+
+3. Test
+
+    `jekyll serve`
+    
+4. Prepare for PDF
+
+    `bash pdf-predstavitev.sh`
+    
+5. Instead of prince work in wkhtmltopdf
+
+    `wkhtmltopdf toc http://127.0.0.1:4000/tocpage.html http://127.0.0.1:4000/titlepage.html http://127.0.0.1:4000/index.html http://127.0.0.1:4000/sklopi.html http://127.0.0.1:4000/podatki.html http://127.0.0.1:4000/uporaba.html http://127.0.0.1:4000/podpora.html D:\test.pdf`
+                 
--- pdf-predstavitev.sh
+++ pdf-predstavitev.sh
@@ -4,19 +4,15 @@ echo 'Kill all Jekyll instances'
 kill -9 $(ps aux | grep '[j]ekyll' | awk '{print $2}')
 clear
 
-#uros: instanca 1 git bash zaženež serve
-
 echo "Building PDF-friendly HTML site for predstavitev ...";
 #bundle exec jekyll serve --detach --config _config.yml,pdfconfigs/config_predstavitev_pdf.yml;
 bundle exec jekyll serve --config _config.yml,pdfconfigs/config_predstavitev_pdf.yml;
 echo "done";
 
-#uros: instanca 2 git bash zaženeš prince
+#echo "Building the PDF ...";
+#prince --javascript --input-list=_site/pdfconfigs/prince-list.txt -o pdf/predstavitev.pdf;
 
-echo "Building the PDF ...";
-prince --javascript --input-list=_site/pdfconfigs/prince-list.txt -o pdf/predstavitev.pdf;
-
-echo "Done. Look in the pdf directory to see if it printed successfully."
+#echo "Done. Look in the pdf directory to see if it printed successfully."
 
 #uros: urediš pdf po potrebi https://www.pdfescape.com (samo link na prvi strani ven, več ne moreš)
 #opcija 2: https://smallpdf.com/ in convert to word in porihtaš
